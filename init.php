173dbae5c6821be952c86ff7982a17afc072de76
Klavdius <39763793+Klavdius@users.noreply.github.com>
2020-06-30 15:42:07 +0300

    csv + json build

--- init.php
+++ init.php
@@ -1,8 +1,9 @@
 <?php
 	echo "Привет о дивный мир <br/>";
+	
 	/*****************************************************************/
 	//прочитать из XML файла данные и использовать их в подключении бд
-	
+	/*
 	$block = simplexml_load_file('settings.xml');
 
 	//echo $block->DB->HOST;
@@ -13,9 +14,19 @@
 	$myDB = $block->DB->DB_NAME;
 	
 	$db = new mysqli($myHost, $myName, $myPass, $myDB);
-	
+	*/
 	/*************************************************************/
 	
+	/**********************************************/
+	// используем для подключения json
+		
+		
+	$jsData = file_get_contents('conf.json');
+	
+	$jsArrData = json_decode($jsData , true);
+	
+	$db = new mysqli($jsArrData["host"] , $jsArrData["name"] , $jsArrData["password"] , $jsArrData["db_name"]);
+	/**********************************************/
 	if( $db->connect_errno){
 		echo "Нет соединения <br/>" . PHP_EOL;
 	}else{
@@ -29,12 +40,16 @@
 	
 	//$res_users = $res_users_n->fetch_all();
 	
+	$fp = fopen('table.csv' , 'w+');
+	
 	for ($i = 1; $i <= $detect_row ; $i++){  
 		
 	$res_users = $res_users_n->fetch_row();  //берем строку и по идеи каждый раз новую
+	fputcsv($fp , $res_users);
 	
 	$flag_id = 0;
 	foreach( $res_users as $value){
+		
 		if ($flag_id == 0){
 			$flag_id = 1;
 		}else{
@@ -44,25 +59,10 @@
 	echo "<br/>";
 	$flag_id = 0;
 	}
-	/*$res_sub = $db->query("SELECT name_sub FROM subscription");
-	$res_room = $db->query("SELECT * FROM rooms");
-	$res_users->data_seek(0);
-	$res_room->data_seek(0);
-	$res_sub->data_seek(0);
-	
-	$sel_id = $db->query("SELECT name FROM users WHERE id_user = 2 ");
-	$name_id = $sel_id->fetch_assoc();
-	echo $name_id['name'] . "<br/>";
-	*/
-	//$first = $res_users->fetch_assoc();
-	//$first_name = $first['id_user'];
-	//INSERT [INTO] card_user(name_user_card) VALUES($row = $
-	/*while($row = $res->fetch_assoc()){
-		echo "Зовут - " . $row['name'] . "<br/>" ;
-	}
-	*/
-	// собрать таблицу "карта" используя запросы из юзера, тарифа и комнаты
 	
 	
+	
+	
+	fclose($fp);
 	
 ?>
