d27f30ca632be0963c3b60aa35598956bb8c7985
Klavdius <39763793+Klavdius@users.noreply.github.com>
2020-07-06 13:35:09 +0300

    finally mysql Select. + write csv file

--- result.php
+++ result.php
@@ -9,7 +9,7 @@ $myChek = htmlentities ($_POST['chek']);
 	 echo "Вы ввели не один <br/>";
 }
 */
-echo $myChek . "<br/>";  //получили переменную
+//echo $myChek . "<br/>";  //получили переменную
 //conect db
 /********************************/
 	$jsData = file_get_contents('conf.json');
@@ -23,32 +23,38 @@ if( $db->connect_errno){
 		echo "Нет соединения <br/>" . PHP_EOL;
 	}else{
 		echo "Подключение к БД <br/>" . PHP_EOL;
+		echo "<br/>";
 	}
 /********************************/
 
 $fileOp = fopen('fullStroka.csv' , 'w+');
 
-$cardUser = $db->query("SELECT * FROM card_user WHERE idCardUser = '$myChek ' "); //получили нужную строку
+
+$cardUser = $db->query("SELECT * FROM cardUser WHERE idCardUser = '$myChek ' "); //получили нужную строку
 $card = $cardUser->fetch_array(MYSQLI_NUM);
 
-fputcsv($fileOp, $card);
+$tyu = 'ага ага, пошел я на ';
+fputcsv($fileOp,array("$card[0]","$card[1]","$card[2]"));
+
+//fputcsv($fileOp, $card);
+fputcsv($fileOp,array("$tyu"));
 //fputcsv($fileOp, $user[0]);
 //fclose($fileOp);
-$a = $card[1];
-$b = $card[2];
-$userName = $mysqlUser->query("SELECT * FORM users WHERE idUser = '$a' ");
-$subUser = $mysqlSub->query("SELECT * FROM subscription WHERE idSub = '$b' ");
 
+
+$userName = $db->query("SELECT * FROM users WHERE idUser = '$card[1]' ");
 $fullName = $userName->fetch_array(MYSQLI_NUM);
+
+$subUser = $db->query("SELECT * FROM subscription WHERE idSub = '$card[2]' ");
 $subMan = $subUser->fetch_array(MYSQLI_NUM);
 
 printf(" %s %s %s с тарифом %s <br/>",$fullName[1],$fullName[2],$fullName[3],$subMan[1]);
 
-//$goCsv = "$fullName['name'],$fullName['lastName'],$fullName['middleName'],$subMan['nameSub']";
-//fptcsv($fileOp, $goCvs);
+fputcsv($fileOp,array("$fullName[1] ","$fullName[2] ","$fullName[3] ", ' с тарифом ', "$subMan[1] "));
+
 fclose($fileOp);
-//$arrayU = mysql_fetch_array($userName , MSQL_ASSOC);
-printf(" %s <br/>", $card['idCardUser']);
+
+
 ?>
 
 
