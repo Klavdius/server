469e4836fcf6307ac0d74ecb8da10d6c8c387f07
Klavdius <39763793+Klavdius@users.noreply.github.com>
2020-07-15 14:08:18 +0300

    Add files via upload

--- join.php
+++ join.php
@@ -8,7 +8,10 @@ $jsArrData = json_decode($jsData, true);
 $db = new mysqli($jsArrData["host"] , $jsArrData["name"] , $jsArrData["password"] , $jsArrData["db_name"]);
 /******/
 
-$joinSelect = $db->query("SELECT * FROM users INNER JOIN subscription using(id)");
+//$joinSelect = $db->query("SELECT * FROM users INNER JOIN subscription using(id)");
+$joinSelect = $db->query("SELECT * FROM carduser"); 
+//LEFT JOIN users ON(name) WHERE users.id = carduser.nameUserCard LEFT JOIN subscription ON(nameSub) 
+//WHERE carduser.subCard = subscription.id  ");
 $join = $joinSelect->fetch_array(MYSQLI_NUM);
 
 
